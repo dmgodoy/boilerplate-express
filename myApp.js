431fa731c8dfec829eb16865bcc896c57784c302
David M <davidmedinagodoy@gmail.com>
2022-07-12 18:32:48 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ console.log("Hello World");
 
 
 
-app.get("/", (req, res) => {res.send(__dirname + "views/index.html")});
+app.get("/", (req, res) => {res.sendFile(__dirname + "/views/index.html")});
 
 
 
