5529e8ab384ac8c5ae296bd0f5f1f18a82df0f97
David M <davidmedinagodoy@gmail.com>
2022-07-12 19:24:24 +0000

    Added middlewareFunction for static resources

--- myApp.js
+++ myApp.js
@@ -2,7 +2,11 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 
+let middlewareFunction = express.static(__dirname + "/public/");
 
+console.log(middlewareFunction);
+
+app.use("/public", middlewareFunction);
 
 
 app.get("/", (req, res) => {res.sendFile(__dirname + "/views/index.html")});
